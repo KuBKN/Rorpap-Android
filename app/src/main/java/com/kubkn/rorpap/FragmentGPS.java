f4b78d2c0229362090a2f7e7d651822e35479450
Poramate Homprakob <batmaster_kn@hotmail.com>
2016-02-24 16:11:00 +0700

    set up drawer and fragments

---
+++ app/src/main/java/com/kubkn/rorpap/FragmentGPS.java
@@ -0,0 +1,59 @@
+package com.kubkn.rorpap;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by batmaster on 2/24/16 AD.
+ */
+public class FragmentGPS extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_gps, container, false);
+
+        final TextView textViewGPS = (TextView) view.findViewById(R.id.textViewGPS);
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textViewGPS.setText(location.getLatitude() + " " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Toast.makeText(getActivity().getApplicationContext(), "Status Change To: " + status , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(getActivity().getApplicationContext(), "GPS is disabled" , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        int resourceId = getArguments().getInt(MyActivity.KEY_DRAWABLE_ID);
+
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/kubkn/rorpap/MainActivity.java
+++ app/src/main/java/com/kubkn/rorpap/MainActivity.java
@@ -1,13 +1,98 @@
 package com.kubkn.rorpap;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
+
+    private String[] mDrawerTitle = {"Login", "GPS"};
+    private DrawerLayout mDrawerLayout;
+    private ListView mListView;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mListView = (ListView) findViewById(R.id.drawer);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mDrawerTitle);
+
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectFragment(position);
+                mDrawerLayout.closeDrawer(mListView);
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
+
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        selectFragment(0);
+    }
+
+    private void selectFragment(int position) {
+        ArrayList<Fragment> fragmentList = new ArrayList<Fragment>();
+        fragmentList.add(new FragmentLogin());
+        fragmentList.add(new FragmentGPS());
+//        Bundle args = new Bundle();
+//        args.putInt(KEY_DRAWABLE_ID, imageResourceIds[position]);
+//        fragment.setArguments(args);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragmentList.get(position));
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
