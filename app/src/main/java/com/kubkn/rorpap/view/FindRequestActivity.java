e727da792b7d3a38f2341a66328cd784f51454c6
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2016-04-13 16:15:17 +0700

    Separate find request to a new view

--- app/src/main/java/com/kubkn/rorpap/view/FindRequestActivity.java
+++ app/src/main/java/com/kubkn/rorpap/view/FindRequestActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,11 +23,10 @@ import android.widget.ListView;
 import com.kubkn.rorpap.R;
 import com.kubkn.rorpap.service.Preferences;
 import com.kubkn.rorpap.service.RorpapApplication;
+import com.kubkn.rorpap.view.fragment.myquest.FindRequest;
 
-/**
- * Created by knotsupavit on 11-Apr-16.
- */
 public class FindRequestActivity extends AppCompatActivity {
+
     private String[] mDrawerTitle = {"Find Request", "Messenger", "Map", "Profile", "Log out"};
     private DrawerLayout mDrawerLayout;
     private ListView mListView;
@@ -30,10 +34,12 @@ public class FindRequestActivity extends AppCompatActivity {
 
     private RorpapApplication app;
 
+    private MyQuestPagerAdapter myQuestPagerAdapter;
+    private ViewPager pager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         app = (RorpapApplication) getApplicationContext();
@@ -109,6 +115,54 @@ public class FindRequestActivity extends AppCompatActivity {
             }
         });
         //TODO: implement 1. show all requests 2. search requests
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(mListView);
+            }
+        });
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        myQuestPagerAdapter = new MyQuestPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(myQuestPagerAdapter);
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+
+        for (int i = 0; i < myQuestPagerAdapter.getCount(); i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(myQuestPagerAdapter.getTag(i))
+                            .setTabListener(tabListener));
+        }
 
     }
 
@@ -120,4 +174,32 @@ public class FindRequestActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private class MyQuestPagerAdapter extends FragmentStatePagerAdapter {
+
+        private Fragment[] fragments = {
+                new FindRequest()
+        };
+
+        private String[] tags = {
+                "Find Request"
+        };
+
+        public MyQuestPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return fragments[position];
+        }
+
+        public String getTag(int position) {
+            return tags[position];
+        }
+    }
 }
